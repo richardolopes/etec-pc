4b935b85024ce972293b37d7bea25f3e828c483a
richardolopes <ri.olive.2007.ro@gmail.com>
2018-02-27 03:58:59 -0300

    Primeira aula de PC

---
+++ aula3/src/ex3/Exercicio3.java
@@ -0,0 +1,8 @@
+package ex3;
+public class Exercicio3 {
+	public static void main( String[] args) {
+		String exer3 = "Curso de Java - Exemplo de String";
+
+		System.out.println("Tamanho da String: " + exer3.length() );
+		}		
+}
