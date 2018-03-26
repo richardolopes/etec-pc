d1d8d87bcd8f6c64973c1b32ab4d972f3b4d30d7
Richard Lopes <ri.olive.2007.ro@gmail.com>
2018-03-26 13:25:33 -0300

    Organizacao3 :+1:

---
+++ 2018.03.06/Projeto-eclipse/Aula4/src/aula4/Teste.java
@@ -0,0 +1,41 @@
+package aula4;
+public class Teste {
+	public static void main(String[] args){
+		// Ex7
+		int raio = 12;
+		double area;
+		double pi = 3.1415;
+		
+		area = pi * Math.pow(raio, 2);
+		// Fim Ex7
+		
+		
+		
+		// Ex8
+		int resto;
+		
+		resto = 99 % 4;
+		// Fim Ex8
+		
+		
+		
+		// Ex9
+		double a = 10;
+		a = (double) (10 * 0.5);
+		
+		System.out.print(a + " ");
+		// Fim Ex9
+		
+		
+		
+		// Ex10
+		int b = 5;
+		for(int multi = 0; multi < 7; multi++){
+			b = b + 5;
+			System.out.print(b + " ");
+		}
+		// Fim Ex10
+		
+		
+	}
+}
